0d23a2cbe83bbc6cce064210b8cad2df133c4762
Drappear <44987855+Drappear@users.noreply.github.com>
2022-04-22 16:46:52 +0900

    sql java bean

---
+++ src/chap14/javaBeans/Customer.java
@@ -0,0 +1,55 @@
+package chap14.javaBeans;
+
+public class Customer {
+	private String customerId;
+	private String name;
+	private String contName;
+	private String address;
+	private String city;
+	private String country;
+	private String postCode;
+	
+	public String getPostCode() {
+		return postCode;
+	}
+	public void setPostCode(String postCode) {
+		this.postCode = postCode;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getCity() {
+		return city;
+	}
+	public void setCity(String city) {
+		this.city = city;
+	}
+	public String getCountry() {
+		return country;
+	}
+	public void setCountry(String country) {
+		this.country = country;
+	}
+	public String getCustomerId() {
+		return customerId;
+	}
+	public void setCustomerId(String customerId) {
+		this.customerId = customerId;
+	}
+	public String getContName() {
+		return contName;
+	}
+	public void setContName(String contName) {
+		this.contName = contName;
+	}
+	public String getAddress() {
+		return address;
+	}
+	public void setAddress(String address) {
+		this.address = address;
+	}
+	
+}
