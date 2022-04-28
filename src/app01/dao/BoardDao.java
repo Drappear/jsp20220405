86a80505d1cd4e3392a78e9a0b5b4f2a99476d9d
Drappear <44987855+Drappear@users.noreply.github.com>
2022-04-28 17:02:58 +0900

    dao

---
+++ src/app01/dao/BoardDao.java
@@ -0,0 +1,93 @@
+package app01.dao;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.Statement;
+import java.sql.Timestamp;
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
+
+import app01.dto.BoardDto;
+
+public class BoardDao {
+	
+	// DTO : Data Transfer Object
+	
+	public boolean insert(Connection con, BoardDto dto) {
+		String sql = "INSERT INTO Board (title, body, inserted) "
+				+ "VALUES (?, ?, ?) ";
+		int result = 0;
+		
+		// statement
+		try(PreparedStatement pstmt = con.prepareStatement(sql)){
+			pstmt.setString(1, dto.getTitle());
+			pstmt.setString(2, dto.getBody());
+			
+			// LocalDate 날짜
+			// LocalDateTime 날짜, 시간
+			LocalDateTime now = LocalDateTime.now();
+			pstmt.setTimestamp(3, Timestamp.valueOf(now));
+			
+			// execute query
+			result = pstmt.executeUpdate();
+		} catch(Exception e) {
+			e.printStackTrace();
+		}
+		
+		// 결과 return
+		return result == 1;
+	}
+
+	public List<BoardDto> list(Connection con) {
+		List<BoardDto> list = new ArrayList<>();
+		
+		String sql = "SELECT id, title, inserted FROM Board ORDER BY id DESC";
+		
+		try(Statement stmt = con.createStatement();
+				ResultSet rs = stmt.executeQuery(sql);) {
+			
+			while(rs.next()) {
+				BoardDto dto = new BoardDto();
+				
+				dto.setId(rs.getInt(1));
+				dto.setTitle(rs.getString(2));
+				dto.setInserted(rs.getTimestamp(3).toLocalDateTime());
+				
+				list.add(dto);
+			}			
+		} catch(Exception e) {
+			e.printStackTrace();
+		}
+		
+		return list;
+	}
+	
+	public BoardDto get(Connection con, int id) {
+		String sql = "SELECT id, title, body, inserted "
+				+ "FROM Board "
+				+ "WHERE id = ?";
+		
+		try (PreparedStatement pstmt = con.prepareStatement(sql)){
+			pstmt.setInt(1, id);
+			
+			try(ResultSet rs = pstmt.executeQuery()) {
+				
+				if(rs.next()) {
+					BoardDto dto = new BoardDto();
+					dto.setId(rs.getInt(1));
+					dto.setTitle(rs.getString(2));
+					dto.setBody(rs.getString(3));
+					dto.setInserted(rs.getTimestamp(4).toLocalDateTime());
+										
+					return dto;
+				}
+			}			
+		} catch(Exception e) {
+			e.printStackTrace();
+		}
+		
+		return null;
+	}
+}
